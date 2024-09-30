e428c9e5c68d7ca98f8eb594d6356c247d74b1e9
SilaDemirkaya <152109535+SilaDemirkaya@users.noreply.github.com>
2024-09-29 19:00:48 -0600

    week-3

--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
+import Link from 'next/link';
+
 
 export default function Home() {
   return (
   <div>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <p><a href="week-2">Week 2</a></p>
+      
+      <nav> 
+      <div><Link href="week-2">Week-2</Link></div>
+      <div><Link href="week-3">Week-3</Link></div>
+      </nav>
   </div>
   );
 }
--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import Link from 'next/link';  
 
-const StudentInfo = () => {
+export default function StudentInfo() {
   return (
-    <div>
-      <h1>Sila Demirkaya</h1>  
-      <p>
-        <Link href="https://github.com/SilaDemirkaya" legacyBehavior> 
-        <a target="_blank">https://github.com</a>  
-        </Link>
-      </p>
-    </div>
+    <>
+      <Name />
+      <GithubLink />
+    </>
   );
-};
+}
 
-export default StudentInfo;  
+// returns a <h1> tag with the Sila's full name
+function Name() {
+  return <h1>Sila Demirkaya</h1>;
+}
+
+// returns a <nav> tag with a link to the Sila's GitHub page
+function GithubLink() {
+  return (
+    <nav>
+      <Link href="https://github.com/SilaDemirkaya">https://github.com</Link>
+    </nav>
+  );
+}
